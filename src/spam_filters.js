0c86c87e25297b98fd12ef5cd2e03aff5be64a56
Christopher Goh <chrisgzf@gmail.com>
2021-09-24 04:34:32 +0800

    Add another spam message

--- src/spam_filters.js
+++ src/spam_filters.js
@@ -4,8 +4,12 @@
  * Deletes messages that contains exactly these terms in its message body.
  * 
  * "finance, statistics, nursing" - to match annoying homework box message
+ * "paid trusted tutors" - same as above
  */
 
-const matchingPattern = ["finance, statistics, nursing"];
+const matchingPattern = [
+  "finance, statistics, nursing",
+  "paid trusted tutors",
+];
 
 module.exports = matchingPattern;
