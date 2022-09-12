48495887f00cee77964e8b84cdc2f319494b05c7
Christopher Goh <chrisgzf@gmail.com>
2022-09-12 13:10:10 +0800

    Update banlist

--- src/banned_users.js
+++ src/banned_users.js
@@ -2,6 +2,6 @@
  * TeleNUS Banned Users
  */
 
-const bannedUsers = new Set([2004668121, 5294371283]);
+const bannedUsers = new Set([994365268, 970102352, 2004668121, 5294371283]);
 
 module.exports = bannedUsers;
