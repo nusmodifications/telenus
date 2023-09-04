cf75911281fb52050c441a8b2a7d6bcd082aa3f0
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2023-09-04 17:07:33 +0800

    Update superusers.js
    Add myself so that I can ban spammers from the telenus groups I manage.

--- src/superusers.js
+++ src/superusers.js
@@ -4,6 +4,6 @@
  * People who can use commands to ban/delete users via TeleNUS.
  */
 
-const superusers = new Set([256297780]);
+const superusers = new Set([256297780, 337648992]);
 
 module.exports = superusers;
